1de4c1f72438cc2acbf40ab135101b9184664488
Andrew Goss <gossfunkel@gmail.com>
2013-06-22 15:45:30 +0100

    add ruby server

---
+++ RubyCitadelServer/Game.rb
@@ -0,0 +1,23 @@
+class Game
+  
+  def initialize
+    puts "Game starting..."
+  end
+  
+  def start
+    running = true
+    run()
+  end
+  
+  def run
+    while(running) 
+      
+    end
+    stop
+  end
+  
+  def stop
+    running = false
+  end
+  
+end
